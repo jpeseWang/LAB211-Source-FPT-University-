f576c1d522196167688643d962f761593ae58435
wang <quang07112004@gmail.com>
2023-09-06 11:08:09 +0700

    Bubble & QuickSort

---
+++ src/Common/Algorithm.java
@@ -0,0 +1,65 @@
+package Common;
+
+public class Algorithm {
+    public void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+
+    public int partition(int[] array, int left, int right) {
+        int i = left, j = right;
+        int pivot = array[(left + right) / 2];
+        int temp;
+        while (i <= j) {
+            while (array[i] < pivot) {
+                i++;
+            }
+            while (array[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        ;
+        return i;
+    }
+
+    public void quickSort(int[] array, int left, int right) {
+        int index = partition(array, left, right);
+        if (left < index - 1) {
+            quickSort(array, left, index - 1);
+        }
+        if (index < right) {
+            quickSort(array, index, right);
+        }
+    }
+
+    public int binarySearch(int[] arr, int target) {
+        int l = 0;
+        int r = arr.length - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2; // Init mid
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return -1; // If target not found
+    }
+}
